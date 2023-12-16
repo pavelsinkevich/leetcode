7c111a4bc9543af242f01c9e8296cd427e023fff
pavel_sinkevich1 <pavel.sinkevich@gmail.com>
2023-12-16 20:15:18 +0200

    More methods for task 189

--- Top interview 150/array and string/189. Rotate Array.py
+++ Top interview 150/array and string/189. Rotate Array.py
@@ -4,6 +4,7 @@ k = 2
 
 
 '''
+#solution 1
 l = len(nums)
 nums_result = []
 if k != 0:
@@ -14,6 +15,8 @@ if k != 0:
 print(nums)
 print(nums_result)
 '''
+'''
+#solution 2
 l = len(nums)
 replaced_indexes = set()
 if k != 0 and l != 0:
@@ -29,4 +32,21 @@ if k != 0 and l != 0:
             current_index = (current_index + 1) % l
             replace_value = nums[(current_index - k + l) % l]
         #replace_index = (current_index + k) % l
+print(nums)
+'''
+'''
+#solution 3
+l = len(nums)
+s = (l-k) % l
+nums[:] = nums[s:l] + nums[0:s]
+print(nums)
+'''
+
+#solution 3
+l = len(nums)
+if k != 0 and l != 0:
+    s = (l-k) % l
+    nums.reverse()
+    nums[:s] = reversed(nums[:s])
+    nums[s:] = reversed(nums[s:])
 print(nums)
