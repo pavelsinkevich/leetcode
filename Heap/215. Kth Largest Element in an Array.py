a8683386e4d3c57350cfc0a027b8d928f3172ec3
pavel_sinkevich1 <pavel.sinkevich@gmail.com>
2023-12-18 15:18:03 +0200

    improved solution for task 215 by using heapq.heapify instead of iteration

--- Heap/215. Kth Largest Element in an Array.py
+++ Heap/215. Kth Largest Element in an Array.py
@@ -1,8 +1,9 @@
 '''Given an integer array nums and an integer k, return the kth largest element in the array.
 Note that it is the kth largest element in the sorted order, not the kth distinct element.
 Can you solve it without sorting?'''
-from heapq import heappop
-from heapq import heappush
+#from heapq import heappop
+#from heapq import heappush
+import heapq
 
 
 class Solution():
@@ -12,13 +13,10 @@ class Solution():
         :type k: int
         :rtype: int
         """
-        priority_queue = []
-        for num in nums:
-            heappush(priority_queue, -num)
-        val = -heappop(priority_queue)
-        for i in range(1, k):
-            val = -heappop(priority_queue)
-        return val
+        heapq.heapify(nums)
+        for i in range(len(nums) - k):
+            heapq.heappop(nums)
+        return nums[0]
 
 nums = [3,2,3,1,2,4,5,5,6]
 k = 4   
