b7037d7bc0588cbc860cedf9d7bff6f778f36e93
pavel_sinkevich1 <pavel.sinkevich@gmail.com>
2023-12-19 12:34:42 +0200

    use second matrix instead of defaultdict. Slightly improved memory usage

--- Daily Challenge/661. Image Smoother.py
+++ Daily Challenge/661. Image Smoother.py
@@ -2,7 +2,7 @@
 and the eight surrounding cells (i.e., the average of the nine cells in the blue smoother). If one or more of the surrounding cells 
 of a cell is not present, we do not consider it in the average (i.e., the average of the four cells in the red smoother).
 '''
-from collections import defaultdict
+#from collections import defaultdict
 class Solution(object):
     def imageSmoother(self, img):
         """
@@ -11,17 +11,25 @@ class Solution(object):
         """
         n = len(img)
         m = len(img[0])
-        cell_neighbors = defaultdict(list)
+        #cell_neighbors = defaultdict(list)
+        smooth_img = []
         for i in range(n):
+            smooth_img.append([])
             for j in range(m):
+                local_sum = 0
+                local_qty = 0
                 for dx in range(-1, 2):
                     for dy in range(-1, 2):
-                        cell_neighbors[(i+dx,j+dy)].append(img[i][j])
-        for i in range(n):
-            for j in range(m):
-                img[i][j] = sum(cell_neighbors[(i, j)]) // len(cell_neighbors[(i, j)])
-
-        return img
+                        if 0<=i+dx<n and 0<=j+dy<m:
+                            local_sum += img[i+dx][j+dy]
+                            local_qty += 1
+                smooth_img[-1].append(local_sum//local_qty)
+        return smooth_img
+                        #cell_neighbors[(i+dx,j+dy)].append(img[i][j])
+        #for i in range(n):
+        #    for j in range(m):
+        #        img[i][j] = sum(cell_neighbors[(i, j)]) // len(cell_neighbors[(i, j)])
+        #return img
         
 img = [[1,1,1],[1,0,1],[1,1,1]]
 obj = Solution()
