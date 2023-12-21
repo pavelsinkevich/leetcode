7e771fa9331a6fdcba475ce4ecc09fbc89ab23dc
pavel_sinkevich1 <pavel.sinkevich@gmail.com>
2023-12-21 12:02:36 +0200

    Added test setup

--- Top interview 150/Linked List/141. Linked List Cycle.py
+++ Top interview 150/Linked List/141. Linked List Cycle.py
@@ -5,10 +5,12 @@ Internally, pos is used to denote the index of the node that tail's next pointer
 
 Return true if there is a cycle in the linked list. Otherwise, return false.'''
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+         self.val = x
+         self.next = None
+    def setNext(self, next):
+        self.next = next
 
 class Solution(object):
     def hasCycle(self, head):
@@ -24,5 +26,15 @@ class Solution(object):
         
 head = [3,2,0,-4]
 pos = 1
+head_obj = []
+for i in range(len(head)):
+    ln = ListNode(head[i])
+    head_obj.append(ln)
+    if i > 0:
+        head_obj[i-1].setNext(head_obj[i])
+    if i == len(head) - 1 and pos != -1:
+        head_obj[i].setNext(head_obj[pos])
+
+
 obj = Solution()
-print(obj.hasCycle(head))
+print(obj.hasCycle(head_obj[0]))
