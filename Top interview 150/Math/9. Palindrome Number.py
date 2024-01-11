02b8dcda4fe810d2491b2d6f18faa582e8d43521
pavel_sinkevich1 <pavel.sinkevich@gmail.com>
2024-01-11 23:56:24 +0200

    Solved task 9

---
+++ Top interview 150/Math/9. Palindrome Number.py
@@ -0,0 +1,12 @@
+'''Given an integer x, return true if x is a palindrome, and false otherwise.'''
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x_str = str(x)
+        for i in range(len(x_str) // 2):
+            if x_str[i] != x_str[len(x_str) - i - 1]:
+                return False
+        return True
+
+obj = Solution()
+x = 121
+print(obj.isPalindrome(x))
